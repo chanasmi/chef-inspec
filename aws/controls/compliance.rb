d59cb2a9d4625457298aa5c78962fc1a3bdf5f32
chanasmi <chanasmie64@gmail.com>
2020-07-14 12:24:44 -0400

    Update compliance.rb

--- aws/controls/compliance.rb
+++ aws/controls/compliance.rb
@@ -1,9 +1,33 @@
 control "aws-1" do
   impact 0.7
-  title 'Checks the tags for EC2 instance'
+  title 'Checks the machine is running'
 
-  describe aws_ec2_instance('i-0c0284127b8c2e752') do
-    its('tags') { should include(key: 'Environment', value: 'Test') }
+  # describe aws_ec2_instance('i-00c32c460bb0a7e0a') do
+  #   its('tags') { should include(key: 'Environment', value: 'Test') }
+  # end
+  aws_ec2_instances.instance_ids.each do |instance_id|
+    describe aws_ec2_instance(instance_id) do
+      it              { should_not have_roles }
+      its('instance_type') { should cmp 't2.micro' }
+      its('key_name') { should cmp 'chef_inspec' }
+      its('image_id') { should eq 'i-0ec7c2f30624be2f9' }
+      its('tags') { should include(key: 'Environment', value: 'Test') }
+    end 
   end
-end
+  # describe aws_ec2_instances do
+  #   its('instance_ids.count') { should cmp 2 }
+  # end
+  # aws_ec2_instances.where( tags: {"Environment" => "Test"}).instance_ids.each do |id|
+  #   describe aws_ec2_instance(id) do
+  #     it { should be_stopped }
+  #   end
+  # end
+  # aws_ec2_instances.where( name: "Test").instance_ids.each do |id|
+  #   describe aws_ec2_instance(id) do
+  #     its('role') { should eq "Cloudwatch" }
+  #   end
+  # end  
+  
 
+
+end
