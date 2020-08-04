8fa4fa2d50c2941b4c64844c1c22c0c0ab1caa3a
chanasmi <chanasmie64@gmail.com>
2020-08-03 22:07:42 -0400

    Update compliance.rb

--- aws/controls/compliance.rb
+++ aws/controls/compliance.rb
@@ -1,7 +1,36 @@
-control "aws-1" do
+control "ec2-instance" do
   impact 0.7
-  title 'Checks the machine is running'
+  title 'Checks the compliance checks for ec2 instance'
+  input('instance_ids').each do |i|
+      describe aws_ec2_instance(i) do
+         it { should exist }
+         its('monitoring') { should eq 'state: "disabled"' }
+         its('vpc_id') { should include 'vpc-' }
+         its('tags') { should include(key: 'Environment', value: 'dev') }
+         its('deletion_protection')     { should eq true }
+         its('image_id') { should eq 'ami-27a58d5c' }
+         its('tenancy') { should eq 'default' }
+         its('instance_type') { should eq 't2.micro' }
+         its('public_ip_address') { should be_nil }
+         its('ebs_encryption') { should eq true }
+         
+       end
+      describe aws_security_group('sg-09d4b7a5dc72d2d11') do
+        it { should allow_in(port: 22, ipv4_range: '10.0.0.0/8') }
+      end
+  end
+  describe aws_s3_bucket(input('bucket')) do
+    it { should exist }
+    it { should_not be_public }
+    #its('tags') { should include(:Environment => 'env-name', :Name => 'bucket-name')}
+    its('versioning') { should eq 'enabled' }
+    its('transfer_acceleration') { should eq 'enabled' }
+    its('default_encryption') { should eq 'enabled' }
+    its('lifecycle') { should eq 'enabled' }
+    its('logging') { should eq 'enabled' }
 
+  end
+end  
   # describe aws_ec2_instance('i-00c32c460bb0a7e0a') do
   #   its('tags') { should include(key: 'Environment', value: 'Test') }
   # end
